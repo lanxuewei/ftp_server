b9ca451a996c51ff022afb56cf1f7fc9025797a1
lanxuewei <lanxuewei@deepwise.com>
2018-10-26 21:03:10 +0800

    新增user相关mapper接口、xml文件以及entity对象

---
+++ ftp_server-service/src/main/java/com/ideasfly/dao/mapper/user/UserMapper.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2017-2018 DeepWise All Rights Reserved.
+ * http://www.deepwise.com
+ */
+package com.ideasfly.dao.mapper.user;
+
+import com.ideasfly.dao.entity.User;
+import tk.mybatis.mapper.common.BaseMapper;
+
+/**
+ * @author lanxuewei Create in 2018/10/26 20:43
+ * Description: UserMapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
