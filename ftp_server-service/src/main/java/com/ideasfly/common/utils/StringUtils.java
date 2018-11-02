ca434ff73ef3bc1ea72429bed17018ff898b0bc6
lanxuewei <lanxuewei@deepwise.com>
2018-11-02 10:04:14 +0800

    新增StringUtils工具类

---
+++ ftp_server-service/src/main/java/com/ideasfly/common/utils/StringUtils.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2017-2018 DeepWise All Rights Reserved.
+ * http://www.deepwise.com
+ */
+package com.ideasfly.common.utils;
+
+import java.util.UUID;
+
+/**
+ * @author lanxuewei Create in 2018/11/1 19:20
+ * Description: String Utils
+ */
+public class StringUtils {
+
+    /**
+     * 私有构造器
+     */
+    private StringUtils() {
+    }
+
+    /**
+     * get uuid
+     * @return uuid
+     */
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+}
