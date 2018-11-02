5f2c45fa1c01427e845c0b37a179b30412bd1c21
lanxuewei <lanxuewei@deepwise.com>
2018-11-02 09:53:40 +0800

    新增配置类扫描位置

--- ftp_server-service/src/main/java/com/ideasfly/main/FtpServerServiceApplication.java
+++ ftp_server-service/src/main/java/com/ideasfly/main/FtpServerServiceApplication.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2017-2018 DeepWise All Rights Reserved.
  * http://www.deepwise.com
  */
-package com.ideasfly;
+package com.ideasfly.main;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
  * Description: service 启动类
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.ideasfly.dao"})
+@ComponentScan(basePackages = {"com.ideasfly.config", "com.ideasfly.dao"})
 public class FtpServerServiceApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(FtpServerServiceApplication.class);
@@ -24,4 +24,5 @@ public class FtpServerServiceApplication {
         SpringApplication.run(FtpServerServiceApplication.class);
         logger.info("================FtpServer Service App  SUCCESS===========");
     }
+
 }
