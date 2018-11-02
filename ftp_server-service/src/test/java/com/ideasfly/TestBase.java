f6aa25acb5d434f634f8ea71908c19aae705668d
lanxuewei <lanxuewei@deepwise.com>
2018-11-02 10:04:25 +0800

    新增单元测试父类

---
+++ ftp_server-service/src/test/java/com/ideasfly/TestBase.java
@@ -0,0 +1,18 @@
+package com.ideasfly; /**
+ * Copyright (c) 2017-2018 DeepWise All Rights Reserved.
+ * http://www.deepwise.com
+ */
+
+import com.ideasfly.main.FtpServerServiceApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author lanxuewei Create in 2018/11/1 19:01
+ * Description: 测试父类
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FtpServerServiceApplication.class)
+public class TestBase {
+}
