13331b87b542ba13c315a88be66264ff2faae111
lanxuewei <lanxuewei@deepwise.com>
2018-11-02 09:53:24 +0800

    调整mapper文件相关

--- ftp_server-service/src/main/java/com/ideasfly/dao/mapper/user/UserMapper.java
+++ ftp_server-service/src/main/java/com/ideasfly/dao/mapper/user/UserMapper.java
@@ -6,10 +6,12 @@ package com.ideasfly.dao.mapper.user;
 
 import com.ideasfly.dao.base.BaseMyMapper;
 import com.ideasfly.dao.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author lanxuewei Create in 2018/10/26 20:43
  * Description: UserMapper
  */
+@Mapper
 public interface UserMapper extends BaseMyMapper<User> {
 }
