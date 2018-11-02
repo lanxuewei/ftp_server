2bf8b760026a4dc192376701cc386fac0e704d9f
lanxuewei <lanxuewei@deepwise.com>
2018-11-02 09:54:49 +0800

    修改建表sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,12 +7,12 @@ USE ftp_server;
 -- 创建user表
 CREATE TABLE IF NOT EXISTS `user`(
   `id` BIGINT AUTO_INCREMENT COMMENT 'id',
-  `user_name` VARCHAR(20) NOT NULL COMMENT '用户名',
-  `password` VARCHAR(32) NOT NULL COMMENT '密码',
+  `user_name` VARCHAR(128) NOT NULL COMMENT '用户名',
+  `password` VARCHAR(64) NOT NULL COMMENT '密码',
   `status` INT NOT NULL DEFAULT 1 COMMENT '权限默认普通用户 0表示管理员 1表示普通用户',
-  `real_name` VARCHAR(20) NOT NULL COMMENT '姓名',
+  `real_name` VARCHAR(128) NOT NULL COMMENT '姓名',
   `sex` INT NOT NULL DEFAULT 1 COMMENT '性别默认男生 0表示女 1表示男',
-  `img` VARCHAR(120) COMMENT '头像',
+  `img` VARCHAR(128) COMMENT '头像',
   `des` TEXT COMMENT '个性语录',
   `create_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
   `update_time` timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '更新时间',
