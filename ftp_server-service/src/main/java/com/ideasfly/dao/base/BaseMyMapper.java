77c60bcb7fe5654695068cf7b3a39d60b4ace891
lanxuewei <lanxuewei@deepwise.com>
2018-11-02 09:52:46 +0800

    修改注释

--- ftp_server-service/src/main/java/com/ideasfly/dao/base/BaseMyMapper.java
+++ ftp_server-service/src/main/java/com/ideasfly/dao/base/BaseMyMapper.java
@@ -3,13 +3,10 @@ package com.ideasfly.dao.base;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
+
 /**
- * 
- * @ClassName: BaseMyMapper 
- * @Description: mybatis的基础Mapper
- * @author LJ 
- * @date 2018年3月8日 下午5:24:55 
- * @param <T>
+ * @author lanxuewei Create in 2018/10/26 20:43
+ * Description: mybatis的基础Mapper
  */
 public interface BaseMyMapper<T> extends Mapper<T>,MySqlMapper<T>{
 
